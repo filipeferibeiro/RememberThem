d2ce368f6103f47651f1d5f353edc7ffd6e1529a
Filipe Fernandes <filipeferibeiro@gmail.com>
2025-07-28 14:28:37 -0300

    Initial Commit

---
+++ RememberThem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RememberThem
+//
+//  Created by Filipe Fernandes on 28/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RememberThem/RememberThemApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberThemApp.swift
+//  RememberThem
+//
+//  Created by Filipe Fernandes on 28/07/25.
+//
+
+import SwiftUI
+
+@main
+struct RememberThemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
